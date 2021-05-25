ba3380fa28c4580f13579e29028bca14048256f5
Danilo Paes <danilopaesdesouza@gmail.com>
2021-05-25 20:05:25 -0300

    add: problems solving May, 25

--- codesign/Smooth Sailing/10. commonCharacterCount.py
+++ codesign/Smooth Sailing/10. commonCharacterCount.py
@@ -1,8 +1,13 @@
-# In progress
+# This was a easy one. First we get the minimum similar char
+# between the two entries and put it into `counter` list
+# and then we just sum :)
 
 def commonCharacterCount(s1, s2):
-    pass
+    
+    items, counter = set(s1), list()
+    
+    for char in items:
+        counter.append(min(s1.count(char), s2.count(char)))
+    
+    return sum(counter)
 
-
-if  __name__ == '__main__':
-    commonCharacterCount("aabcc","adcaa")
