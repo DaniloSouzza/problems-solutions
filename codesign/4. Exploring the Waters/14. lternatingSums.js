c07eabcc8da6eca4da20e5e9e919df9c4da31165
Danilo Paes <danilopaesdesouza@gmail.com>
2021-05-26 23:29:05 -0300

    add: problems solving May, 26

---
+++ codesign/4. Exploring the Waters/14. lternatingSums.js
@@ -0,0 +1,18 @@
+/* Inserting some javascript to pratice :) */
+
+function alternatingSums(a) {
+
+    let team1 = 0;
+    let team2 = 0;
+   
+    for(let i = 0;i < a.length; i++){
+
+      if(i % 2 === 0){
+        team1 += a[i]
+      }else{
+        team2 += a[i]
+      }
+
+  }
+  return [team1, team2]
+}
