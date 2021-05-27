e14f8c90304cb19761ad5035295d483cc8ef1213
Danilo Paes <danilopaesdesouza@gmail.com>
2021-05-27 18:22:50 -0300

    add: problems solving May, 27

--- codesign/4. Exploring the Waters/15. addBorder.py
+++ codesign/4. Exploring the Waters/15. addBorder.py
@@ -2,13 +2,13 @@ def addBorder(picture):
 
     portrait = []
 
-    starline = (len(picture[0])+2)*"*"
+    starline = (len(picture[0])+2) * "*"
 
     portrait.append(starline)
 
     for i in range(len(picture)):
         portrait.append('*'+picture[i]+'*')
-        
+
     portrait.append(starline)
     
     return portrait
